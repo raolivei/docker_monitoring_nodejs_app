004bd2662add875b847fa5b1d1c934c2154a53c6
raolivei <rafa.oliveira1@gmail.com>
2018-08-18 01:32:38 -0300

    added support to cluter number of processors

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-cluster = require 'cluster'
-numCPUs = require('os').cpus().length
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 var express = require('express');
 var app = express();
-                         
-                         
-if (cluster.isMaster)
-    console.log 'I am the master, launching workers!'     
-    cluster.fork() for i in [0...numCPUs]
 
-else                     
-    app.get('/', function (req, res) {
-      res.send('Hello World!');
+if (cluster.isMaster) {
+    // Fork workers.
+      console.log ('I am the MASTER');
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    
+        cluster.on('exit', function(worker, code, signal) {
+        console.log('worker ' + worker.process.pid + ' died');
     });
-    app.listen(3000, function () {
-      console.log('Example app listening on port 3000!');
-    });
+
+
+} else {
+        console.log('I am a WORKER');
+//piece of code from chaordic
+        app.get('/', function (req, res) {
+        res.send('Hello World!');
+        });
+        app.listen(3000, function () {
+          console.log('Example app listening on port 3000!');
+        });
+}
