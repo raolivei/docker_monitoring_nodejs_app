22ba660eaaf6f414efbb37e3749906ff8e5ad958
raolivei <rafa.oliveira1@gmail.com>
2018-09-02 14:49:41 -0300

    changing ports in docker-compose, updating README.md, removing old Grafana services-monitor, adding iperf container.

--- mail/getMail.sh
+++ mail/getMail.sh
@@ -2,7 +2,8 @@
 
 echo "Subject: Daily app-website report" > mail.txt
 echo "" >> mail.txt
-echo "Hello World from Docker Host!, The lines beelow have the data you want:" >> mail.txt
+echo "Este é um relatório simples que mostra a frequência das requisições no website e o respectivo código de resposta  :" >> mail.txt
 echo "" >> mail.txt
+echo "Frequencia | Código de resposta" >> mail.txt
 sudo docker exec -t nginx-proxy  awk '{print $9}' /etc/nginx/logs/access.log | sort | uniq -c | sort -rn >> mail.txt
 cat mail.txt | /usr/sbin/sendmail rafa.oliveira1@gmail.com
