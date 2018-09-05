73f49276d300c0910e860b106be0852f39a4e914
raolivei <rafa.oliveira1@gmail.com>
2018-09-05 11:05:12 -0300

    fixing crontab by removing sudo from command

--- mail/cronJob.sh
+++ mail/cronJob.sh
@@ -7,7 +7,7 @@ Hour=$(date --date='10 minutes' +%H)
 # crontab job to schedule daily emails
 MAILDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 crontab -l | grep -v 'getMail.sh'  | crontab -
-crontab -l | { cat; echo "$Min     $Hour       *       *       *       sudo $MAILDIR/getMail.sh"; } | crontab -
+crontab -l | { cat; echo "$Min     $Hour       *       *       *       $MAILDIR/getMail.sh"; } | crontab -
 
 
 
--- mail/getMail.sh
+++ mail/getMail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DESTINATION_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-echo "Subject: CARAAAAI" > $DESTINATION_PATH/mail.txt
+echo "Subject: Daily app website report" > $DESTINATION_PATH/mail.txt
 echo "" >> $DESTINATION_PATH/mail.txt
 echo "Este é um relatório simples que mostra a frequência das requisições no website e o respectivo código de resposta  :" >> $DESTINATION_PATH/mail.txt
 echo "" >> $DESTINATION_PATH/mail.txt
